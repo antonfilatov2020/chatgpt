d6aa020763e7d571a062a4be0483dee931385eb9
antonfilatov2020 <59093514+antonfilatov2020@users.noreply.github.com>
2025-01-19 16:04:00 +0300

    Update st.js

--- st.js
+++ st.js
@@ -16,7 +16,7 @@ export async function main() {
     apiKey: "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"
   });
 
-  const response = await client.chat.completions.create({
+  const response = await client.models.create({
     messages: [
       { role:"system", content: "" },
       { role:"user", content: "What is the capital of France?" }
